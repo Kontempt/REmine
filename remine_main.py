1b01cd33b15ae8030820e4fd9c249f2f942a1279
Kontempt <oobola@yahoo.com>
2017-10-09 19:58:37 +0300

    Update remine_main.py
    Added capability to run a parse by pulling details (e.g. main source url, xpath values, source name,...)

--- remine_main.py
+++ remine_main.py
@@ -13,8 +13,14 @@ def indv_content_get(article_info, new_details):
 
         article_author = tree.xpath(article_info[0])
         article_body = tree.xpath(article_info[1])
-        print(article_author)
-        print(retools.word_analyzer(article_body))
+
+        article_details = []
+        article_details.append([article_author[0], article_body])
+        #TP:
+        # print(article_author)
+        # print(retools.word_analyzer(article_body))
+        # print(articles_details_all)
+    return article_details
 
 def content_get(src):
     '''Main function that starts up the remine project'''
@@ -27,9 +33,14 @@ def content_get(src):
         title, urllink = retools.tree_parse(src, tree, source[1:3])
 
         new_details = retools.filter_title(title, urllink)
-        # holds litof relevant articles
-        print("TP - NEW DETAILS: ", new_details)
-        indv_content_get(source[3:5], new_details)
+        print("TP:\nSOURCE:\n\t", source[5])
+        # holds list of relevant articles
+        print("NEW DETAILS:\n\t ", new_details)
+        
+        if new_details:
+            article_details_all = indv_content_get(source[3:5], new_details)
+        else:
+            print("NO relevant articles found")
 
     except requests.exceptions.HTTPError:
         print("Page not accessed")
@@ -37,4 +48,6 @@ def content_get(src):
     except requests.exceptions.ConnectionError:
         print("Unable to connect due to NETWORK ERROR.")
 
-content_get(0)
+for key in resource.SOURCE:
+    content_get(key)
+
